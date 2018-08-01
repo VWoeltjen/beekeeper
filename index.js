a75e0a66490bb86c75da3f8a2572997419036be4
Vic Woeltjen <vwoeltjen@gmail.com>
2018-07-31 18:46:11 -0700

    Serve a static index page.

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var options = {
   contract: "0x4d8fc1453a0f359e99c9675954e656d80d996fbf"
 };
 
+var express = require('express');
+var app = express();
+
 var Web3 = require('web3');
 var web3 = new Web3();
+
+
 
 // From https://github.com/ethereum/wiki/wiki/Contract-ERC20-ABI
 var erc20 = require('./bee.json');
@@ -20,3 +25,6 @@ web3.eth.call({
 }).catch(function (error) {
   console.log(error);
 });
+
+app.use(express.static('static'));
+app.listen(8080);
