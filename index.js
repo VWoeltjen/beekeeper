5ab5d50ca206158e28f10dd0745e541c7dad6d50
Vic Woeltjen <vwoeltjen@gmail.com>
2018-08-01 12:07:10 -0700

    Narrative logging.

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ db.allDocs({
   return response.rows.length > 0 ? 
     response.rows[0].doc.event.blockNumber + 1 : options.epoch;
 }).then(function (start) {
+  console.log("Updating event database...");
   return web3.eth.getBlock('latest').then(function (block) {
     return retrieveEvents(start, block.number);
   });
