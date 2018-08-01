856214fea33f33ff4f63edb62d2328bb5a977839
Vic Woeltjen <vwoeltjen@gmail.com>
2018-07-31 18:29:22 -0700

    Use Bee token ABI

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var Web3 = require('web3');
 var web3 = new Web3();
 
 // From https://github.com/ethereum/wiki/wiki/Contract-ERC20-ABI
-var erc20 = require('./erc20.json');
+var erc20 = require('./bee.json');
 var token = new web3.eth.Contract(erc20, options.contract);
 
 web3.setProvider(new web3.providers.HttpProvider(options.url));
 
 web3.eth.call({
   to: options.contract,
-  data: token.methods.balanceOf(options.contract).encodeABI()
+  data: token.methods.name().encodeABI()
 }).then(function (result) {
   console.log(result);
 }).catch(function (error) {
