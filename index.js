c1f87d3f826088803529506aa5ca39e74d34bf8c
Vic Woeltjen <vwoeltjen@gmail.com>
2018-07-31 20:06:00 -0700

    Get Hello Bee Token working from node...

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ var options = {
   contract: "0x4d8fc1453a0f359e99c9675954e656d80d996fbf"
 };
 
+try {
+  var overrides = require('./options.json');
+  Object.keys(overrides).forEach(function (key) {
+    options[key] = overrides[key];
+  });
+} catch (e) {
+  // ...use default options when options.json is not present
+}
+
 var express = require('express');
 var app = express();
 
 var Web3 = require('web3');
 var web3 = new Web3();
 
-
-
-// From https://github.com/ethereum/wiki/wiki/Contract-ERC20-ABI
-var erc20 = require('./static/bee.json');
-var token = new web3.eth.Contract(erc20, options.contract);
+var abi = require('./static/bee.json');
+var token = new web3.eth.Contract(abi, options.contract);
 
 web3.setProvider(new web3.providers.HttpProvider(options.url));
 
-web3.eth.call({
-  to: options.contract,
-  data: token.methods.name().encodeABI()
-}).then(function (result) {
-  console.log(result);
-}).catch(function (error) {
-  console.log(error);
+token.methods.name().call().then(function (name) {
+  console.log(name);
 });
 
-app.use(express.static('static'));
-app.listen(8080);
+// app.use(express.static('static'));
+// app.listen(8080);
